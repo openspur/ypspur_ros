ddb367bafe8f26cb495404ba46937abf71727321
Atsushi Watanabe <atsushi.w@ieee.org>
2016-09-11 10:56:46 +0900

    joint_position_to_joint_trajectory: skips duplicated joint command

--- src/joint_position_to_joint_trajectory.cpp
+++ src/joint_position_to_joint_trajectory.cpp
@@ -14,6 +14,7 @@ private:
 	std::map<std::string, double> state;
 
 	double accel;
+	bool skip_same;
 
 	void cbJointState(const sensor_msgs::JointState::ConstPtr& msg)
 	{
@@ -22,8 +23,52 @@ private:
 			state[msg->name[i]] = msg->position[i];
 		}
 	}
+	ypspur_ros::JointPositionControl cmd_prev;
 	void cbJointPosition(const ypspur_ros::JointPositionControl::ConstPtr& msg)
 	{
+		do
+		{
+			if(msg->joint_names.size() != cmd_prev.joint_names.size()) break;
+			{
+				bool eq = true;
+				for(unsigned int i = 0; i < msg->joint_names.size(); i ++)
+				{
+					if(msg->joint_names[i].compare(cmd_prev.joint_names[i]) != 0) eq = false;
+				}
+				if(!eq) break;
+			}
+			if(msg->positions.size() != cmd_prev.positions.size()) break;
+			{
+				bool eq = true;
+				for(unsigned int i = 0; i < msg->positions.size(); i ++)
+				{
+					if(msg->positions[i] != cmd_prev.positions[i]) eq = false;
+				}
+				if(!eq) break;
+			}
+			if(msg->velocities.size() != cmd_prev.velocities.size()) break;
+			{
+				bool eq = true;
+				for(unsigned int i = 0; i < msg->velocities.size(); i ++)
+				{
+					if(msg->velocities[i] != cmd_prev.velocities[i]) eq = false;
+				}
+				if(!eq) break;
+			}
+			if(msg->accelerations.size() != cmd_prev.accelerations.size()) break;
+			{
+				bool eq = true;
+				for(unsigned int i = 0; i < msg->accelerations.size(); i ++)
+				{
+					if(msg->accelerations[i] != cmd_prev.accelerations[i]) eq = false;
+				}
+				if(!eq) break;
+			}
+			return;
+		}
+		while(0);
+		cmd_prev = *msg;
+		
 		trajectory_msgs::JointTrajectory cmd;
 		cmd.header = msg->header;
 		cmd.joint_names = msg->joint_names;
@@ -59,6 +104,7 @@ public:
 				std::string("joint_trajectory"), 5, false);
 
 		nh.param("accel", accel, 0.3);
+		nh.param("skip_same", skip_same, true);
 	}
 };
 
