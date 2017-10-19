a4b3fb96117daf4056c35d6fc1ab877372ab061f
Atsushi Watanabe <atsushi.w@ieee.org>
2017-10-19 11:28:41 +0900

    Fix coding style.

--- src/joint_tf_publisher.cpp
+++ src/joint_tf_publisher.cpp
@@ -1,51 +1,80 @@
+/*
+ * Copyright (c) 2016-2017, the mcl_3dl authors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its 
+ *       contributors may be used to endorse or promote products derived from 
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <ros/ros.h>
 
-#include <sensor_msgs/JointState.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <sensor_msgs/JointState.h>
 
 #include <tf/transform_broadcaster.h>
 
-#include <sys/types.h>
 #include <signal.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-
+#include <map>
+#include <string>
 
 class joint_tf_publisher_node
 {
 private:
-	ros::NodeHandle nh;
-	std::map<std::string, ros::Publisher> pubs;
-	std::map<std::string, ros::Subscriber> subs;
-	tf::TransformBroadcaster tf_broadcaster;
+  ros::NodeHandle nh;
+  std::map<std::string, ros::Publisher> pubs;
+  std::map<std::string, ros::Subscriber> subs;
+  tf::TransformBroadcaster tf_broadcaster;
 
-	void cbJoint(const sensor_msgs::JointState::ConstPtr& msg)
-	{
-		for(size_t i = 0; i < msg->name.size(); i ++)
-		{
-			geometry_msgs::TransformStamped trans;
-			trans.header = msg->header;
-			trans.header.frame_id = msg->name[i] + "_in";
-			trans.child_frame_id = msg->name[i] + "_out";
-			trans.transform.rotation = tf::createQuaternionMsgFromYaw(msg->position[i]);
-			tf_broadcaster.sendTransform(trans);
-		}
-	}
+  void cbJoint(const sensor_msgs::JointState::ConstPtr& msg)
+  {
+    for (size_t i = 0; i < msg->name.size(); i++)
+    {
+      geometry_msgs::TransformStamped trans;
+      trans.header = msg->header;
+      trans.header.frame_id = msg->name[i] + "_in";
+      trans.child_frame_id = msg->name[i] + "_out";
+      trans.transform.rotation = tf::createQuaternionMsgFromYaw(msg->position[i]);
+      tf_broadcaster.sendTransform(trans);
+    }
+  }
 
 public:
-	joint_tf_publisher_node():
-		nh("~")
-	{
-		subs["joint"] = nh.subscribe("joint", 1, &joint_tf_publisher_node::cbJoint, this);
-	}
+  joint_tf_publisher_node()
+    : nh("~")
+  {
+    subs["joint"] = nh.subscribe("joint", 1, &joint_tf_publisher_node::cbJoint, this);
+  }
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	ros::init(argc, argv, "joint_tf_publisher");
+  ros::init(argc, argv, "joint_tf_publisher");
 
-	joint_tf_publisher_node jp;
-	ros::spin();
+  joint_tf_publisher_node jp;
+  ros::spin();
 
-	return 0;
+  return 0;
 }
-
