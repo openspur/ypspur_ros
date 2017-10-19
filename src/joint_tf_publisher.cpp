c8976ee54879d02c1807c0c97d3606cac35c83cc
Atsushi Watanabe <atsushi.w@ieee.org>
2017-10-19 11:42:10 +0900

    Fix copyright.

--- src/joint_tf_publisher.cpp
+++ src/joint_tf_publisher.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, the mcl_3dl authors
+ * Copyright (c) 2015-2017, the ypspur_ros authors
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
