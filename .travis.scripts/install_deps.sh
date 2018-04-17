77571edddd2f9d7abf9843dcfdf1409836e42669
Atsushi Watanabe <atsushi.w@ieee.org>
2018-04-17 23:10:15 +0900

    Remove unused apt package

--- .travis.scripts/install_deps.sh
+++ .travis.scripts/install_deps.sh
@@ -9,7 +9,6 @@ source /catkin_ws/devel/setup.bash
 cd /catkin_ws
 
 apt-get -qq update
-apt-get install libxml2-utils
 rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
 apt-get clean
 rm -rf /var/lib/apt/lists/*
