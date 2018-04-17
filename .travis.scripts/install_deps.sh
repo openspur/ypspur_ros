f0caf24948568c8672b2a2923b68310eb6e66bce
Atsushi Watanabe <atsushi.w@ieee.org>
2018-04-17 23:27:15 +0900

    Fix skipkeys

--- .travis.scripts/install_deps.sh
+++ .travis.scripts/install_deps.sh
@@ -8,7 +8,15 @@ source /catkin_ws/devel/setup.bash
 
 cd /catkin_ws
 
+SKIP_KEYS=
+if [ -f $scriptdir/rosdepkeys.skip ];
+then
+  SKIP_KEYS="--skip-keys=\"$(cat $scriptdir/rosdepkeys.skip | tr '\n' ' ' | sed 's/\s*$//g')\""
+fi
+
+echo $SKIP_KEYS
+
 apt-get -qq update
-rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
+rosdep install --from-paths src --ignore-src $SKIP_KEYS --rosdistro=${ROS_DISTRO} -y
 apt-get clean
 rm -rf /var/lib/apt/lists/*
