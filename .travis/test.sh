9820b5889b6271ba169b3f897e77ab57c74db023
Atsushi Watanabe <atsushi.w@ieee.org>
2020-04-06 20:14:43 +0900

    Fix test script

--- .travis/test.sh
+++ .travis/test.sh
@@ -10,28 +10,27 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 cd /catkin_ws
 
 CMI_OPTION="--install-space /opt/ros/${ROS_DISTRO} --install"
-CMI_ONLY_PKG="--pkg ${PACKAGE_NAME}"
 
 catkin_make_isolated $CMI_OPTION || \
   (gh-pr-comment "${BUILD_LINK} FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
-catkin_make_isolated $CMI_OPTION $CMI_ONLY_PKG --catkin-make-args tests || \
+catkin_make_isolated $CMI_OPTION --catkin-make-args tests || \
   (gh-pr-comment "${BUILD_LINK} FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
-catkin_make_isolated $CMI_OPTION $CMI_ONLY_PKG --catkin-make-args run_tests || \
+catkin_make_isolated $CMI_OPTION --catkin-make-args run_tests || \
   (gh-pr-comment "${BUILD_LINK} FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
 
 if [ catkin_test_results ];
 then
   result_text="
 \`\`\`
-`catkin_test_results --all | grep -v Skipping || true`
+$(catkin_test_results --all | grep -v Skipping || true)
 \`\`\`
 "
 else
   result_text="
 \`\`\`
-`catkin_test_results --all || true`
+$(catkin_test_results --all || true)
 \`\`\`
-`find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;'`
+$(find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;')
 "
 fi
 catkin_test_results || (gh-pr-comment "${BUILD_LINK} FAILED on ${ROS_DISTRO}" "Test failed$result_text"; false)
